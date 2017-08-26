94859773a850268c846b90cf791ccc1449cb2dff
han8909227 <hbao2016@hotmail.com>
2017-08-25 23:07:43 -0700

    l

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -134,6 +134,8 @@ articleView.initIndexPage = function() {
 };
 
 articleView.initAdminPage = function() {
+  var template = Handlebars.compile($('#admin-template').text());
+
   // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
